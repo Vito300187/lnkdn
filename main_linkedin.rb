af6d9e28239f5a051b9384d80a63559a7f372894
Vitalii Kuzminov <vkuzminov@artec-group.com>
2021-02-13 02:03:02 +0300

    Refactorign spec_helper

--- main_linkedin.rb
+++ main_linkedin.rb
@@ -3,13 +3,17 @@
 require_relative 'spec_helpers'
 
 feature 'Test' do
-  scenario 'for check connecting people' do
-    search_people = Pages::Search::People.new
+  let(:home_page) {Pages::LinkedinHomePage.new}
+  let(:login_page) {Pages::LoginPage.new}
+  let(:user_home_page) {Pages::UserHomePage.new}
+  let(:search_job_page) {Pages::Search::Jobs.new}
+  let(:search_people) {Pages::Search::People.new}
 
-    Pages::LinkedinHomePage.new.move_to_sigh_in
-    Pages::LoginPage.new.sign_in
-    Pages::UserHomePage.new.visit_to_nav_link('Jobs')
-    Pages::Search::Jobs.new.search_job_and_location
+  scenario 'for check connecting people' do
+    home_page.move_to_sigh_in
+    login_page.sign_in
+    user_home_page.visit_to_nav_link('Jobs')
+    search_job_page.search_job_and_location
     search_people.choose_location
     search_people.connect_while_hr_available
 
